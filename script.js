0bfe397ee4abc6f0f9d15b5a5df0b6974ed043fe
bnolanhussle <155196261+bnolanhussle@users.noreply.github.com>
2024-04-17 22:03:41 +0000

    Apr 17, 2024, 10:03 PM

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault();
+          event.stopPropagation();
         }
         // The form is valid so show the modal
         else {
-            displayModal();
+          displayModal();
+          event.preventDefault();
+          event.stopPropagation();
         }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
 
-  function displayModal() {
-    let modal = new bootstrap.Modal(document.getElementById("submitModal"), {
-        backdrop: true,
-        focus: true,
-        keyboard: true
-    });
-    submitModal.show();
-  }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+function displayModal() {
+  let modal = new bootstrap.Modal(document.getElementById("submitModal"), {
+    backdrop: true,
+    focus: true,
+    keyboard: true,
+  });
+  submitModal.show();
+}
